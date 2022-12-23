331f290de539637982c9cdf7c2897f1a3da82f7e
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-12-23 03:33:14 +0100

    Add task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import classRoom from "./0-classroom";
+export default function initializeRooms() {
+    const room1 = new classRoom(19);
+    const room2 = new classRoom(20);
+    const room3 = new classRoom(34);
+    const rooms = [room1, room2, room3];
+    return rooms;
+}
