8402dc4eca30eab436cfcc15225febef40d96e3c
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-12-23 03:55:20 +0100

    Add task 2

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,4 @@
-export default class classRoom {
-  /* eslint-disable no-underscore-dangle */
+export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
-import classRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
-    const room1 = new classRoom(19);
-    const room2 = new classRoom(20);
-    const room3 = new classRoom(34);
-    const rooms = [room1, room2, room3];
-    return rooms;
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  const rooms = [room1, room2, room3];
+  return rooms;
 }
