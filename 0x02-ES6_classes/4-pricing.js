faa785bd885060c0c52c94086747dba9b5de7d45
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-12-23 05:46:12 +0100

    Fix: bugs and code errors

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,31 +1,27 @@
-import Currency from './3-currency';
-
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
-  }
-
-  set amount(amount) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('amount must be a number');
-    }
-    this._amount = amount;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
   set currency(currency) {
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('currency must be an instance of Currency');
-    }
     this._currency = currency;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
