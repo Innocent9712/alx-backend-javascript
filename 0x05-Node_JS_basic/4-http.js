51e6cf8f6221ff3ebac134e72bb46a1de17b3499
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-02-23 23:04:56 +0100

    Add task 1 - 5

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+server.listen(1245, () => {});
